51acad195d197e5b6ba9214f9fb5727e8d47507b
Salome Gene <digsach2003@gmail.com>
2016-10-18 14:19:37 -0400

    Agrego las mejoras para la vista y el controlador

--- src/main/java/com/okiimport/app/mvvm/controladores/configuracion/EditarPerfilViewModel.java
+++ src/main/java/com/okiimport/app/mvvm/controladores/configuracion/EditarPerfilViewModel.java
@@ -1,5 +1,7 @@
 package com.okiimport.app.mvvm.controladores.configuracion;
 
+
+import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
@@ -8,6 +10,7 @@ import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.util.media.Media;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.EventQueues;
 import org.zkoss.zk.ui.event.UploadEvent;
 import org.zkoss.zk.ui.select.annotation.Wire;
 import org.zkoss.zul.Button;
@@ -19,34 +22,56 @@ import com.okiimport.app.mvvm.AbstractRequerimientoViewModel;
 
 
 public class EditarPerfilViewModel extends AbstractRequerimientoViewModel implements EventListener<UploadEvent> {
-	
+
 	//Servicios
-	
+
 	//GUI
 	@Wire("#imgFoto")
 	private Image imgFoto;
-	
+
 	@Wire("#btnCambFoto")
 	private Button btnCambFoto;
-	
+
 	@Wire("#txtClaveNueva")
 	private Textbox txtClaveNueva;
-	
+
 	@Wire("#txtClaveNuevaConf")
 	private Textbox txtClaveNuevaConf;
 	
+	@Wire("#txtUsername")
+	private Textbox txtUsername;
+
+	@Wire("#txtNombre")
+	private Textbox txtNombre;
+
+	@Wire("#txtApellido")
+	private Textbox txtApellido;
+
+	@Wire("#txtDireccion")
+	private Textbox txtDireccion;
+	
+	@Wire("#closeFoto")
+	private Component closeFoto;
+	
+
 	//Modelos
 	private Usuario usuario;
-	
+
 	//Atributos
-	
+	private boolean isValidFoto;
+
 	@AfterCompose
 	public void doAfterCompose(@ContextParam(ContextType.VIEW) Component view){
 		super.doAfterCompose(view);
-		
 		btnCambFoto.addEventListener("onUpload", this);
-		
 		usuario = super.getUsuario();
+		if(usuario.getFoto64()!=null && !usuario.getFoto64().isEmpty()){
+			setValidFoto(true);
+			closeFoto.setVisible(true);
+		} else {
+			setValidFoto(false);
+			closeFoto.setVisible(false);
+		}
 	}
 
 	/**INTERFACES*/
@@ -55,37 +80,116 @@ public class EditarPerfilViewModel extends AbstractRequerimientoViewModel implem
 	public void onEvent(UploadEvent event) throws Exception {
 		// TODO Auto-generated method stub
 		Media media = event.getMedia();
-		if (media instanceof org.zkoss.image.Image)
+		if (media instanceof org.zkoss.image.Image){
 			imgFoto.setContent((org.zkoss.image.Image) media);
-		else if (media != null)
+			closeFoto.setVisible(true);
+			setValidFoto(true);
+		}else if (media != null){
 			mostrarMensaje("Error", "No es una imagen: " + media, null, null, null, null);
+			closeFoto.setVisible(false);
+			setValidFoto(false);
+		}
 	}
-	
+
 	/**COMMAND*/
 	@Command
 	@NotifyChange("usuario")
 	public void enviar(){
+		String severidad ="";
+		String msg="";
 		String nuevaClave = txtClaveNueva.getValue();
 		String nuevaClaveConf = txtClaveNuevaConf.getValue();
 		org.zkoss.image.Image foto = this.imgFoto.getContent();
 		
-		if(foto!=null)
+		if(foto!=null && isValidFoto())
 			usuario.setFoto(foto.getByteData());
 		
-		if(!(nuevaClave.equalsIgnoreCase("") && nuevaClaveConf.equalsIgnoreCase(""))){ //Arreglar
-			if(nuevaClave.equalsIgnoreCase(nuevaClaveConf)){
-				usuario.setPasword(nuevaClave);
-				usuario=sControlUsuario.actualizarUsuario(usuario, true);
-			}
-			else
-				mostrarMensaje("Error", "Las Contraseñas no son iguales", null, null, null, null);
-		}
-		else
-			usuario=sControlUsuario.actualizarUsuario(usuario, false);
+		usuario.getPersona().setApellido(txtApellido.getValue());
+		usuario.getPersona().setNombre(txtNombre.getValue());
+		usuario.getPersona().setDireccion(txtDireccion.getValue());
 		
-		mostrarMensaje("Informacion", "Datos Guardados Satisfactoriamente", null, null, null, null);
+		if (!txtUsername.getValue().equalsIgnoreCase("")){
+			try {
+				if (sControlUsuario.verificarUsername(txtUsername.getValue())
+						& !(usuario.getUsername().equalsIgnoreCase(txtUsername.getValue()))){
+					severidad = "Error";
+					msg ="El username ya se encuentra registrado";
+				}else{
+					if(!(nuevaClave.equalsIgnoreCase("") && nuevaClaveConf.equalsIgnoreCase(""))){
+						if((nuevaClave.equals(nuevaClaveConf))){
+							if(verificarContraseña(nuevaClaveConf)){
+								usuario.setPasword(nuevaClave);
+								usuario=sControlUsuario.actualizarUsuario(usuario, false);
+								severidad = "Informacion";
+								msg ="Datos guardados satisfactoriamente";
+							}else{
+								severidad = "Error";
+								msg ="La contraseña debe contener al menos un número y una letra mayúscula";
+							}
+						}else{
+							severidad = "Error";
+							msg ="Las contraseñas no coinciden, por favor verifique";
+						}
+					}else{
+						BindUtils.postGlobalCommand("perfil", EventQueues.APPLICATION, "updateProfile", null);	
+						usuario=sControlUsuario.actualizarUsuario(usuario, false);
+						severidad = "Informacion";
+						msg ="Datos guardados satisfactoriamente";
+					}
+				}
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+				severidad="Error";
+				msg="Error interno";
+			}
+		}else{
+			severidad = "Error";
+			msg ="Campos obligatorios vacios";
+		}
+			mostrarMensaje(severidad, msg, null, null, null, null);
+			txtClaveNueva.setValue("");
+			txtClaveNuevaConf.setValue("");
+		
+	}
+	
+	/**
+	 * Metodo para limpiar los campos de texto de la vista
+	 */
+	@Command
+	public void limpiar(){
 		txtClaveNueva.setValue("");
 		txtClaveNuevaConf.setValue("");
+		txtNombre.setValue("");
+		txtApellido.setValue("");
+		txtDireccion.setValue("");
+		txtUsername.setValue("");
+	}
+
+	/**
+	 * Metodo que verifica si el formato de la contraseña es correcto
+	 * @param clave contraseña a verificar
+	 * @return true si el formato es correcto, false caso contrario
+	 */
+	public boolean verificarContraseña(String clave){
+		boolean m=false;
+		boolean n=false;
+		for(char x: clave.toCharArray()){
+			if(Character.isUpperCase(x)){
+				m=true;
+			}
+			if(Character.isDigit(x)){
+				n=true;
+			}
+		}
+		return n && m?true:false;
+	}
+	
+	@Command
+	@NotifyChange("usuario")
+	public void eliminarFoto(){
+		usuario.setFoto(null);
+		closeFoto.setVisible(false);
+		setValidFoto(true);
 	}
 	
 	/**SETTERS Y GETTERS*/	
@@ -95,5 +199,13 @@ public class EditarPerfilViewModel extends AbstractRequerimientoViewModel implem
 
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
+	}
+	
+	public boolean isValidFoto() {
+		return isValidFoto;
+	}
+
+	public void setValidFoto(boolean isValidFoto) {
+		this.isValidFoto = isValidFoto;
 	}
 }
